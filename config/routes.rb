2c4fb0dbb649a4ed53213ebf93e3392cb1e06a44
Erick Saavedra <esvdrah@gmail.com>
2021-11-04 16:20:45 -0500

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/welcome', to: 'home#index'
 
+  root to: 'home#index'
+
   get 'articles/new', to: 'articles#new'
   get 'articles/:id', to: 'articles#show'
   get 'articles/:id/edit', to: 'articles#edit'
