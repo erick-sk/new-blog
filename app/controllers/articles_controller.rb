e4f427afc8172c8c666eeb64419e8989d098d72f
Erick Saavedra <esvdrah@gmail.com>
2021-11-04 17:04:39 -0500

    add index to all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, except: [:new, :create, :index]
+
+  def index
+    @articles = Article.all
+  end
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(title: params[:article][:title], content: params[:article][:content])
-
     redirect_to @article
   end
 
@@ -24,8 +26,11 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.fin(params[:id])
     @article.destroy
     redirect_to root_path
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
+  get 'articles', to: 'articles#index'
   get 'articles/new', to: 'articles#new'
   get 'articles/:id', to: 'articles#show'
   get 'articles/:id/edit', to: 'articles#edit'
   patch 'articles/:id', to: 'articles#update', as: :article
   post 'articles', to: 'articles#create'
   delete 'articles/:id', to: 'articles#destroy'
+
 end
