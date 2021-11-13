f1401911c1606bb30597f6235b763dbd9cc304f1
Erick Saavedra <esvdrah@gmail.com>
2021-11-13 17:06:11 -0500

    add avatar to user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'welcome', to: 'home#index'
+  get 'profile', to: 'users#edit'
+  resources :users, only: [:update]
 
   root to: 'home#index'
   resources :categories
